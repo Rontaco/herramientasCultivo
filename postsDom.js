6b5db442a1ad2914809c23136f8dcba6c1e2f967
Roberto Perino <robertusa1999@gmail.com>
2024-11-03 19:35:44 -0300

    Agregado de posts.html con manejo dinamico del html

---
+++ postsDom.js
@@ -0,0 +1,27 @@
+import { posts } from './postsData.js';
+
+// Selecciona el contenedor en el HTML donde se mostrarán los posts
+const postsContainer = document.getElementById("postsContainer");
+
+// Itera sobre el array de posts y genera el HTML para cada uno
+posts.forEach((post) => {
+    // Crea el contenedor para el post
+    const postElement = document.createElement("div");
+    postElement.classList.add("post", "col-md-4", "mb-3");
+
+    // Genera el contenido del post
+    postElement.innerHTML = `
+        <div class="card">
+            <a href="${post.link}" target="_blank">
+                <img src="${post.imageUrl}" class="card-img-top" alt="${post.title}">
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${post.title}</h5>
+                <p class="card-text">Fecha: ${post.date}</p>
+            </div>
+        </div>
+    `;
+
+    // Agrega el post al contenedor principal en el HTML
+    postsContainer.appendChild(postElement);
+});
